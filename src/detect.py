ed4a55b22e89e35eb47d2d592f69d65835569145
Hirushiharan Thevendran <hirushiharant@gmail.com>
2024-02-06 09:00:48 +0530

    feat: return base64 image

--- src/detect.py
+++ src/detect.py
@@ -2,7 +2,7 @@
 Program: OpenCV Identify Numbers
 Author: Hirushiharan Thevendran
 Created On: 01.02.2024
-Last Modified On: 04.02.2024
+Last Modified On: 06.02.2024
 
 Programe Description: Python application using the OpenCV library to identify numbers in a video stream.
 File Description: Contains function to read the camera input and identify the numbers using HSV image processing.
@@ -12,10 +12,19 @@ Python Version: 3.10
 
 import cv2
 import numpy as np
+import base64
 
 
 def identify_num(l_h, l_s, l_v, u_h, u_s, u_v):
-    cap = cv2.VideoCapture(0)
+
+    print("********************************** L_H: ", l_h)
+    print("********************************** L_S: ", l_s)
+    print("********************************** L_V: ", l_v)
+    print("********************************** U_H: ", u_h)
+    print("********************************** U_S: ", u_s)
+    print("********************************** U_V: ", u_v)
+
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
     while True:
         _, frame = cap.read()
@@ -29,4 +38,8 @@ def identify_num(l_h, l_s, l_v, u_h, u_s, u_v):
 
         res = cv2.bitwise_and(frame, frame, mask=mask)
 
-        return res
+        # Convert the processed frame to base64 encoding
+        _, buffer = cv2.imencode('.jpg', res)
+        res_base64 = base64.b64encode(buffer)
+
+        return res_base64.decode('utf-8')

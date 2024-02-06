6b39ab6bc48d9a6622e95a186151e60847b6bb32
Hirushiharan Thevendran <hirushiharant@gmail.com>
2024-02-06 09:00:00 +0530

    fix: web streaming issue

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 Program: OpenCV Identify Numbers
 Author: Hirushiharan Thevendran
 Created On: 01.02.2024
-Last Modified On: 04.02.2024
+Last Modified On: 06.02.2024
 
 Programe Description: Python application using the OpenCV library to identify numbers in a video stream.
 File Description: Flask application to for API functions.
@@ -15,8 +15,12 @@ from detect import identify_num
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, supports_credentials=True)
 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/identify_numbers', methods=['POST'])
 def start_opencv():
@@ -31,10 +35,10 @@ def start_opencv():
         res = identify_num(lh_value, ls_value, lv_value,
                            uh_value, us_value, uv_value)
 
-    except Exception as e:
-        res = jsonify({"error": str(e)})
+        return jsonify({"results": res})
 
-    return jsonify({"results": res})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
 if __name__ == '__main__':
